9cf5ed3eb41c14e332253205dfa851e273ef6d57
MFatih OĞUL <72889065+mfatih000@users.noreply.github.com>
2023-07-19 17:05:51 +0300

    added new features
    requested features added

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-let inputAdd=document.getElementById("inputAdd");
-let toDoContainer=document.getElementById("toDoContainer");
-let inputText=document.getElementById("inputText");
-let inputClear=document.getElementById("inputClear");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const Button = document.getElementById("removebutton");
 
-inputAdd.addEventListener("click",function(){
-    let paragraph= document.createElement("p");
-    paragraph.classList.add("paragraph-style");
-    toDoContainer.appendChild(paragraph);
-    paragraph.innerHTML=inputText.value;
-    inputText.value="";
-    paragraph.addEventListener("click",function(){
-        paragraph.style.textDecoration="line-through";
-    })
-    paragraph.addEventListener("dblclick",function(){
-        toDoContainer.removeChild(paragraph);
-    })
-    inputClear.addEventListener("click",function(){
-        paragraph.remove();
-    })
-})
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You have to write something");
+    }
+
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
 
 
+    save();
+}
 
+function clearInput() {
+    inputBox.value = '';
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        save();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        save();
+    }
+}, false);
+
+function removeAll() {
+    while (listContainer.firstChild) listContainer.removeChild(listContainer.firstChild);
+}
+
+function save() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function show() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+show();
